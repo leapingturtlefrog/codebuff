4fbd4b1f1bbdeb62d9ee735a899d5513f8deb972
James Grugett <jahooma@gmail.com>
2025-09-26 23:35:03 -0700

    [buffbench] base max!!

--- evals/git-evals/run-eval-set.ts
+++ evals/git-evals/run-eval-set.ts
@@ -72,7 +72,7 @@ class RunEvalSetCommand extends Command {
     }),
     agent: Flags.string({
       description: 'Codebuff agent id to use',
-      default: 'base-lite',
+      default: 'base-max',
     }),
     help: Flags.help({ char: 'h' }),
   }
